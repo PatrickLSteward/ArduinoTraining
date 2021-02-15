09a722cf8fa014d74d21043fcce875e532c95b61
vyuansmu <56751006+vyuansmu@users.noreply.github.com>
2021-02-15 14:48:16 -0600

    Update setup.c
    incorperated tempature conversion functions into steup

--- setup.c
+++ setup.c
@@ -1,8 +1,24 @@
 // General setup File
 
+//Tempature Functions headers
+float celToFrah(float);
+float frahToCel(float);
+
+
 int pot = A0; // potentionmeter analog pin
+
 
 void setup()
 {
   pinMode(A0, INPUT);
+}
+
+float celToFrah(float Celsius) {
+    float Fahrenheit = (9.0/5.0)*(Celsius) + 32.0; 
+    return Fahrenheit;
+}
+
+float frahToCel(float Fahrenheit) {
+    float Celsius = (5.0/9.0)*(Fahrenheit - 32.0);
+    return Celsius;
 }
