0838ca9900505451a4d9be2df59f8ccd4cab4e72
Jinto2021 <78880395+Jinto2021@users.noreply.github.com>
2021-02-15 15:41:31 -0600

    Update warning_alert.c
    Added buzzer functionality

--- warning_alert.c
+++ warning_alert.c
@@ -1,11 +1,12 @@
 // #include "settings.h"
 
 
-
+int buzzer_tone_val = 500; // modify this value to change output tone/ frequency
 int alert_status=0;
+int warning_Buzzer = 9; // 8-ohm speaker on digital pin 9
 int warning_LED = 13; // pin number assigned tentativel. may need to change depending on LCD connection.
 
-int getStatus(); // reurns a corresponding alert flag for a specified threshold
+int getStatus(); // returns a corresponding alert flag for a specified threshold (defined elsewhere)
   //  - 0 = normal
   //  - 1 = high (alert)
 
@@ -16,7 +17,8 @@ int getStatus(); // reurns a corresponding alert flag for a specified threshold
 
 void setup()
 {
-  pinMode(warning_LED, OUTPUT);
+  pinMode(warning_LED, OUTPUT); // Warning LED  connected to pin 'warning_LED'
+  pinMode(warning_Buzzer, OUTPUT); // 8-ohm speaker on digital pin 'warning_Buzzer'
 }
 
 void loop()
@@ -27,6 +29,7 @@ alert_status = getStatus;
 
 if (alert_status==1)
   {
+  // LED ON
   digitalWrite(warning_LED, HIGH);  // LED turned ON
   
                     /* Blinking mode
@@ -37,7 +40,11 @@ if (alert_status==1)
                     digitalWrite(warning_LED, LOW);
                     delay(500); // Wait for 500 millisecond(s)
                     */
+  // BUZZER ON 
   
+  // map the sensor reading to a range for the speaker
+  tone(9, 440 * pow(2.0, (constrain(int(map(buzzer_tone_val, 0, 1023, 36, 84)), 35, 127) - 57) / 12.0), 1000);
+  delay(10); // Delay a little bit to improve simulation performance
     }
 else
   {
@@ -45,3 +52,6 @@ else
   }
   
 }
+
+
+
