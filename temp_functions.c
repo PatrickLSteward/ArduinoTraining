43b1655c2aa112c4160b44f157730127436a812b
vyuansmu <56751006+vyuansmu@users.noreply.github.com>
2021-02-15 15:55:43 -0600

    Update temp_functions.c
    match the change to setup.c

--- temp_functions.c
+++ temp_functions.c
@@ -1,10 +1,12 @@
 float celToFrah(float Celsius) {
     float Fahrenheit = (9.0/5.0)*(Celsius) + 32.0; 
+    mode = 'F'; // sets mode to Fahrenheit
     return Fahrenheit;
 }
 
 
 float frahToCel(float Fahrenheit) {
     float Celsius = (5.0/9.0)*(Fahrenheit - 32.0);
+    mode = 'C'; // sets mode to Celsius
     return Celsius;
 }
